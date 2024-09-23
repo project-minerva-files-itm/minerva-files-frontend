976c867c7fac6df407032e804aabe8ea61ca9ca3
brayanmedinacardozo96 <brayanmedinacardozo@gmail.com>
2024-09-23 11:14:32 -0500

    feat: host-app

--- setting-spa/vite.config.ts
+++ setting-spa/vite.config.ts
@@ -1,10 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: "setting-components",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./List": "./src/components/List.jsx",
+        "./Input": "./src/components/Input.jsx",
+      },
+      shared: ["react"],
+    }),
+  ],
   build: {
     outDir: 'dist',
     sourcemap: true,
